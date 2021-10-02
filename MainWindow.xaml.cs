c01c96a2dc30ccd0155b1aadbb1b62fb2fe8236b
Aidas Baublys <aidas.baublys.darbo@gmail.com>
2021-10-02 19:29:31 +0300

    Start project with Linq to SQL data context, add method to insert data into table

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace Link_to_SQL
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        LinqToSQLDataContext dataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string cs = ConfigurationManager.ConnectionStrings["Link_to_SQL.Properties.Settings.EchoDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSQLDataContext(cs);
+
+            InsertUni("MRU");
+        }
+
+        public void InsertUni(string name)
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University uni = new University();
+            uni.Name = name;
+            dataContext.Universities.InsertOnSubmit(uni);
+            dataContext.SubmitChanges();
+
+            MainData.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudent()
+        {
+            University uni = dataContext.Universities.First(u => u.Name.Equals("MRU"));
         }
     }
 }
